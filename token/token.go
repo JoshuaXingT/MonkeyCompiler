f552b6508316339973a8a3d1f189c79406371b9e
120090162 <85002296+120090162@users.noreply.github.com>
2023-05-17 21:13:57 +0800

    initialize

---
+++ token/token.go
@@ -0,0 +1,41 @@
+package token
+
+type TokenType string
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+	// VARIABLE
+	IDENT = "IDENT"
+	INT   = "INT"
+	// OPERATOR
+	ASSIGN = "="
+	PLUS   = "+"
+	// SEPARATOR
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+	// KEY WORDS
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
